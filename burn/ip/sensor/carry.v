0062f7c831e6c4d8a34ea45b5d77365a51077cfe
Dustin Richmond <6378612+drichmond@users.noreply.github.com>
2022-10-22 16:53:40 -0700

    More BSG modules, ready for DPI integration

--- burn/ip/sensor/carry.v
+++ burn/ip/sensor/carry.v
@@ -1,22 +1,17 @@
-`timescale 1ns / 1ps
-
 module carry
   #(
     parameter C_OUT_WIDTH=1024
-    ,parameter C_SYNC_DEPTH = 2
+    ,parameter C_DEPTH_SYNC = 2
     ,parameter C_FAMILY = "7-Series")
    (
-    input                    S_AXIS_TVALID,
-    input [7:0]              S_AXIS_TDATA,
-    output                   S_AXIS_TREADY,
-    input                    S_AXIS_ACLK,
-    input                    S_AXIS_ARESETN,
-    output [C_OUT_WIDTH-1:0] M_AXIS_TDATA,
-    output                   M_AXIS_TVALID,
-    output                   M_AXIS_TLAST,
-    input                    M_AXIS_TREADY,
-    input                    M_AXIS_ACLK,
-    input                    M_AXIS_ARESETN
+    input		     pulse_v
+    ,input		     pulse
+    ,input		     pulse_clk
+    ,output [C_OUT_WIDTH-1:0] carry_o
+    ,output		     valid_o
+    // TODO: ASSERT WHEN NOT READY
+    ,input		     ready_i
+    ,input		     capture_clk
     );
 
    localparam                CARRY_WIDTH_LP = 4;
@@ -38,25 +33,25 @@ module carry
    wire [C_OUT_WIDTH:0]      wResult;
 
    (* keep = "true" *)
-   reg [C_OUT_WIDTH-1:0] rSync[C_SYNC_DEPTH-1:0];
+   reg [C_OUT_WIDTH-1:0] rSync[C_DEPTH_SYNC-1:0];
 
    (* keep = "true" *)
-   reg                   rSyncValid[C_SYNC_DEPTH-1:0];
+   reg                   rSyncValid[C_DEPTH_SYNC-1:0];
 
    wire [C_OUT_WIDTH-1:0]    wSyncEnd;
 
    // In the sensor, we are always ready. We have to be, to have continuous data.
-   assign S_AXIS_TREADY = 1;
+   // assign S_AXIS_TREADY = 1;
 
    // TODO: Assert if SYNC depth is less than 2
-   assign M_AXIS_TLAST = (M_AXIS_TVALID & !rSyncValid[C_SYNC_DEPTH-2]);
-   assign M_AXIS_TVALID = rSyncValid[C_SYNC_DEPTH-1];
+   // assign M_AXIS_TLAST = (valid_o & !rSyncValid[C_DEPTH_SYNC-2]);
+   assign valid_o = rSyncValid[C_DEPTH_SYNC-1];
 
    // Carry Logic:
    // All 1's, static input
    assign wInputB = {{C_OUT_WIDTH{1'b1}}};
    // All 1's, except the last bit.
-   assign wInputA = {{C_OUT_WIDTH-1{1'b1}}, S_AXIS_TDATA[0]};
+   assign wInputA = {{C_OUT_WIDTH-1{1'b1}}, pulse};
 
    // Initial cascade in
    assign wCascadeIn[0] = 1'b0;
@@ -103,27 +98,27 @@ module carry
    // Synchronizer logic for data & valid
    xpm_cdc_array_single
      #(
-       .DEST_SYNC_FF(C_SYNC_DEPTH),
+       .DEST_SYNC_FF(C_DEPTH_SYNC),
        ,.SIM_ASSERT_CHK(1)
        ,.SRC_INPUT_REG(0)
        ,.WIDTH (C_OUT_WIDTH)
        )
    syncgen
      (
-      .src_clk(S_AXIS_ACLK)
+      .src_clk(pulse_clk)
       ,.src_in(wResult[C_OUT_WIDTH-1:0])
-      ,.dest_clk(M_AXIS_ACLK)
-      ,.dest_out(M_AXIS_TDATA)
+      ,.dest_clk(capture_clk)
+      ,.dest_out(carry_o)
       );
 
    // Synchronize the valid bit
-   always @(posedge M_AXIS_ACLK) begin
-      rSyncValid[0] <= S_AXIS_TVALID;
+   always @(posedge capture_clk) begin
+      rSyncValid[0] <= pulse_v;
    end
 
    generate
-      for(i = C_SYNC_DEPTH-1 ; i > 0; i = i - 1) begin : syncgen_v
-         always @(posedge M_AXIS_ACLK) begin
+      for(i = C_DEPTH_SYNC-1 ; i > 0; i = i - 1) begin : syncgen_v
+         always @(posedge capture_clk) begin
             rSyncValid[i] <= rSyncValid[i-1];
          end
       end
